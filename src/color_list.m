539d872598cdfd314c0059b3775b7ccf0f9a733f
Chongyang Du <2389883115@qq.com>
2022-09-17 21:25:30 +0800

    add src folder

---
+++ src/color_list.m
@@ -0,0 +1,15 @@
+% collect the wonderful colormap
+function color_list()
+
+% from 10.1038/s41467-017-02374-7
+% fig. 3j
+lee_color_1 =  [133 6   33
+                234 136 107
+                245 246 248
+                111 175 211
+                13  62  121];
+
+end
+
+
+
